bbd5372af9da9da274bfec9d7f8aad86eb6d79d1
Abdullah Qamardeen Olamiteju <qamardeenabdullah@gmail.com>
2026-03-27 04:08:54 -0700

    Be_Techifiedwk3

--- app.js
+++ app.js
@@ -5,19 +5,42 @@ app.use(express.json()); // Parse JSON bodies
 let todos = [
   { id: 1, task: 'Learn Node.js', completed: false },
   { id: 2, task: 'Build CRUD API', completed: false },
+  { id: 3, task: 'Get only one todo', completed: true},
+  { id: 4, task: 'post task(required)', completed: true },
+  { id: 5, task: 'Get active task', completed: false },
 ];
 
 // GET All – Read
 app.get('/todos', (req, res) => {
-  res.status(200).json(todos); // Send array as JSON
+  res.status(200).json(todos);
 });
+
+
+// GET completed – Custom Read
+app.get('/todos/active', (req, res) => {
+  const completed = todos.filter((t) => t.completed === false); // Array.filter() – non-destructive
+  res.json(completed); // Custom Read!
+});
+
+
+// GET specific – Read
+app.get('/todos/:id', (req, res) => {
+  const todo = todos.find((t) => t.id === parseInt(req.params.id));
+  if (!todo) return res.status(404).json({ message: 'Todo not found' });
+  res.status(200).json(todo);
+});
+
 
 // POST New – Create
 app.post('/todos', (req, res) => {
   const newTodo = { id: todos.length + 1, ...req.body }; // Auto-ID
   todos.push(newTodo);
+  if (!newTodo.task) {
+    return res.status(400).json({ error: 'Task is required' });
+  }
   res.status(201).json(newTodo); // Echo back
 });
+
 
 // PATCH Update – Partial
 app.patch('/todos/:id', (req, res) => {
@@ -26,6 +49,7 @@ app.patch('/todos/:id', (req, res) => {
   Object.assign(todo, req.body); // Merge: e.g., {completed: true}
   res.status(200).json(todo);
 });
+
 
 // DELETE Remove
 app.delete('/todos/:id', (req, res) => {
@@ -37,14 +61,11 @@ app.delete('/todos/:id', (req, res) => {
   res.status(204).send(); // Silent success
 });
 
-app.get('/todos/completed', (req, res) => {
-  const completed = todos.filter((t) => t.completed);
-  res.json(completed); // Custom Read!
-});
 
 app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Server error!' });
 });
 
 const PORT = 3002;
+
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
